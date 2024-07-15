4381cfca335209870f3e8de0c9ca094cf708770f
Naofumi Kagami <naofumi@mac.com>
2024-07-15 18:00:36 +0900

    Improve sleep: use env.RESPONSE_DELAY, and do not delay internal network requests

--- middleware.ts
+++ middleware.ts
@@ -5,9 +5,24 @@ import {sleep} from "@/helpers/sleep";
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
-  await sleep(500)
+  if (shouldDelayResponse(request)) {
+    const responseDelay = parseInt(process.env.RESPONSE_DELAY || "")
+    await sleep(responseDelay)
+  }
 
   return NextResponse.next()
+}
+
+function shouldDelayResponse(request: NextRequest) {
+  const userAgent = request.headers.get('user-agent')
+  if (userAgent === "node") {
+    // If the userAgent is "node" then this request originated from
+    // the Next.js server and is an internal connection. In this case,
+    // we expect very low latency.
+    return false
+  } else {
+    return true
+  }
 }
 
 // See "Matching Paths" below to learn more
