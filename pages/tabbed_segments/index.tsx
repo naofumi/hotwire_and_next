5d329f5c65bf6b2e9f0a90077a212c51dcfad16f
Naofumi Kagami <naofumi@mac.com>
2024-07-16 10:52:23 +0900

    wording fix

--- pages/tabbed_segments/index.tsx
+++ pages/tabbed_segments/index.tsx
@@ -26,8 +26,7 @@ export default function TabbedSegmentsIndexPage() {
                 TurboFrames</a>
               <Link href="/tabbed_segments"
                     className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                Next.jsによる
-                タブ切り替え</Link>
+                Next.js useEffect</Link>
             </div>
             <div className="mt-10">
               <div className="mb-2">
