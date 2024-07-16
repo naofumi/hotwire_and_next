33cf10c0aa8c6940da291ee1ec89e14a728c9f2f
Naofumi Kagami <naofumi@mac.com>
2024-07-16 15:06:48 +0900

    Add time to popup to show caching and revalidation

--- pages/components/popup/UserPopup.tsx
+++ pages/components/popup/UserPopup.tsx
@@ -19,37 +19,41 @@ export default function UserPopup({id}: { id: number }) {
       {
         !user
           ? <div className="w-48">
-            <Image src={rocketImage} alt="loader" className="m-auto w-16 h-16" />
-        </div>
+            <Image src={rocketImage} alt="loader" className="m-auto w-16 h-16"/>
+          </div>
           : user
-            ? <table>
-              <tbody>
-              <tr>
-                <td className="font-bold pr-3">Name:</td>
-                <td className="text-right">{user.name}</td>
-              </tr>
-              <tr>
-                <td className="font-bold pr-3">Title:</td>
-                <td className="text-right">{user.title}</td>
-              </tr>
-              <tr>
-                <td className="font-bold pr-3">Email:</td>
-                <td className="text-right">{user.email}</td>
-              </tr>
-              <tr>
-                <td className="font-bold pr-3">Hobby:</td>
-                <td className="text-right">{user.hobby}</td>
-              </tr>
-              <tr>
-                <td className="font-bold pr-3">Joined At:</td>
-                <td className="text-right">{user.joinedAt}</td>
-              </tr>
-              <tr>
-                <td className="font-bold pr-3">Job Grade:</td>
-                <td className="text-right">{user.jobGrade}</td>
-              </tr>
-              </tbody>
-            </table>
+            ?
+            <>
+              <div className="mb-2">{new Date().toLocaleTimeString()}</div>
+              <table>
+                <tbody>
+                <tr>
+                  <td className="font-bold pr-3">Name:</td>
+                  <td className="text-right">{user.name}</td>
+                </tr>
+                <tr>
+                  <td className="font-bold pr-3">Title:</td>
+                  <td className="text-right">{user.title}</td>
+                </tr>
+                <tr>
+                  <td className="font-bold pr-3">Email:</td>
+                  <td className="text-right">{user.email}</td>
+                </tr>
+                <tr>
+                  <td className="font-bold pr-3">Hobby:</td>
+                  <td className="text-right">{user.hobby}</td>
+                </tr>
+                <tr>
+                  <td className="font-bold pr-3">Joined At:</td>
+                  <td className="text-right">{user.joinedAt}</td>
+                </tr>
+                <tr>
+                  <td className="font-bold pr-3">Job Grade:</td>
+                  <td className="text-right">{user.jobGrade}</td>
+                </tr>
+                </tbody>
+              </table>
+            </>
             : null
       }
     </>
