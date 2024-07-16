b369e6b5c39be7a1f67ae06bc56f8f447d15bb5f
Naofumi Kagami <naofumi@mac.com>
2024-07-16 16:22:52 +0900

    Stimulus

---
+++ pages/api/hotwire/popup_stimulus/users.ts
@@ -0,0 +1,16 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import {render} from "@/helpers/template-renderer"
+
+import {allUsers} from "@/repositories/user";
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<string>,
+) {
+  const resultText = render("popup_stimulus/users.ejs", {users: allUsers()})
+
+  res.appendHeader("Content-Type", "text/html")
+    .status(200)
+    .send(resultText)
+}
---
+++ public/hotwire/javascript/popup_controller.js
@@ -0,0 +1,15 @@
+import {Controller} from "/hotwire/javascript/stimulus.js"
+
+export default class extends Controller {
+  static targets = ["popupWindow"]
+
+  showPopup(event) {
+    this.hideAllPopups()
+    this.popupWindowTarget.classList.remove('hidden')
+  }
+
+  hideAllPopups() {
+    document.querySelectorAll("[data-js=user-detail-popup]")
+      .forEach((popup) => popup.classList.add('hidden'))
+  }
+}
