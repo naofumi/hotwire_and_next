b6d80821eac796ec1bda5fdc59a3685c1f66efa2
Naofumi Kagami <naofumi@mac.com>
2024-07-17 15:10:45 +0900

    app router pages for page transitions
    remove console.log

--- app/products_app/page.tsx
+++ app/products_app/page.tsx
@@ -1,14 +1,14 @@
-import {allUsers} from "@/repositories/user";
 import Link from "next/link";
+import {allProducts} from "@/repositories/product";
 
 export const dynamic = 'force-dynamic'
 
- async function getUsers() {
-   return allUsers()
+ async function getProducts() {
+  return allProducts()
 }
 
-export default async function UsersAppIndex() {
-  const users = await getUsers()
+export default async function ProductsAppIndex() {
+  const products = await getProducts()
   const time = new Date().toLocaleTimeString()
 
   return (
@@ -17,7 +17,7 @@ export default async function UsersAppIndex() {
         <div className="sm:flex sm:items-center">
           <div className="sm:flex-auto">
             <div>{time}</div>
-            <h1 className="text-base font-semibold leading-6 text-gray-900">Users</h1>
+            <h1 className="text-base font-semibold leading-6 text-gray-900">Products</h1>
           </div>
         </div>
         <div className="mt-8 flow-root">
@@ -31,46 +31,46 @@ export default async function UsersAppIndex() {
                     Name
                   </th>
                   <th scope="col"
-                      className="px-4 py-3.5 text-left text-sm font-semibold text-gray-900">Title
+                      className="px-4 py-3.5 text-left text-sm font-semibold text-gray-900">Catalog Number
                   </th>
                   <th scope="col"
-                      className="px-4 py-3.5 text-left text-sm font-semibold text-gray-900">Email
+                      className="px-4 py-3.5 text-left text-sm font-semibold text-gray-900">Price
                   </th>
                   <th scope="col"
                       className="py-3.5 pl-4 pr-4 text-left text-sm font-semibold text-gray-900 sm:pr-0">
-                    Role
+                    Availability
                   </th>
                 </tr>
                 </thead>
                 <tbody className="divide-y divide-gray-200 bg-white">
-                {users.map((user, i) => (
+                {products.map((product, i) => (
                   <tr key={i} className="divide-x divide-gray-200">
                     <td
                       className="whitespace-nowrap py-4 pl-4 pr-4 text-sm font-medium text-gray-900 sm:pl-0">
-                      {user.name}
+                      {product.name}
                     </td>
                     <td className="whitespace-nowrap p-4 text-sm text-gray-500">
-                      {user.title}
+                      {product.catalogNumber}
                     </td>
                     <td
                       className="whitespace-nowrap p-4 text-sm text-gray-500">
-                      {user.email}
+                      {product.price}
                     </td>
                     <td
                       className="whitespace-nowrap py-4 pl-4 pr-4 text-sm text-gray-500 sm:pr-0">
-                      {user.role}
+                      {product.availability}
                     </td>
                   </tr>))}
                 </tbody>
               </table>
               <div className="my-10 flex items-center justify-center gap-x-6">
-                <Link href="/products_app"
+                <Link href="/users_app"
                       className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                  Productsへ App router w/o prefetch</Link>
-                <Link href="/products_app"
+                  Usersへ App router w/o prefetch</Link>
+                <Link href="/users_app"
                       prefetch={true}
                       className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                  Productsへ App router w/ prefetch</Link>
+                  Usersへ App router w/ prefetch</Link>
               </div>
             </div>
           </div>
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    // https://zenn.dev/akfm/articles/nextjs-cache-default-update
+    staleTimes: {
+      dynamic: 0, // default: 0
+      static: 300, // default: 300
+    },
+  },
+};
 
 export default nextConfig;
