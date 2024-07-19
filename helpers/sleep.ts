4e4730b34be75a904a87c65ac5da651330e324ee
Naofumi Kagami <naofumi@mac.com>
2024-07-19 21:02:02 +0900

    add databaseSleep() and responseSleep()

--- helpers/sleep.ts
+++ helpers/sleep.ts
@@ -1,3 +1,14 @@
+export const databaseDelay = parseInt(process.env.DATABASE_DELAY || "0")
+export const responseDelay = parseInt(process.env.RESPONSE_DELAY || "0")
+
 export function sleep(ms: number) {
   return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+export async function databaseSleep() {
+  await sleep(databaseDelay)
+}
+
+export async function responseSleep() {
+  await sleep(responseDelay)
 }
