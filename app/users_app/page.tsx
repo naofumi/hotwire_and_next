f34a02db5e9d7751e5495564bb8cc1717679a349
Naofumi Kagami <naofumi@mac.com>
2024-07-19 17:06:25 +0900

    formatting

--- app/users_app/page.tsx
+++ app/users_app/page.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 
 export const dynamic = 'force-dynamic'
 
- async function getUsers() {
-   return allUsers()
+async function getUsers() {
+  return allUsers()
 }
 
 export default async function UsersAppIndex() {
