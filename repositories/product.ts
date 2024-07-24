eb89a9dc13bc354b9f389e075e74d87fb9557079
Naofumi Kagami <naofumi@mac.com>
2024-07-24 21:02:58 +0900

    typo fix

--- repositories/product.ts
+++ repositories/product.ts
@@ -9,7 +9,7 @@ export type Product = {
 
 const products = [
   {name: "Chili Potatoes", catalogNumber: "15413234", price: 200, availability: "On Sale"},
-  {name: "Rock hard Senbei", catalogNumber: "5624523", price: 400, availability: "Discontinues"},
+  {name: "Rock hard Senbei", catalogNumber: "5624523", price: 400, availability: "Discontinued"},
   {name: "Fruity Jelly", catalogNumber: "125476435", price: 300, availability: "On Sale"},
   {name: "Chocolate Bucket", catalogNumber: "5234526234", price: 1000, availability: "On Sale"},
   {name: "Happy Biscuit", catalogNumber: "1325345", price: 600, availability: "On Sale"},
@@ -19,7 +19,7 @@ const products = [
   {name: "Chocolate Bucket", catalogNumber: "5234526234", price: 1000, availability: "On Sale"},
   {name: "Happy Biscuit", catalogNumber: "1325345", price: 600, availability: "On Sale"},
   {name: "Chili Potatoes", catalogNumber: "15413234", price: 200, availability: "On Sale"},
-  {name: "Rock hard Senbei", catalogNumber: "5624523", price: 400, availability: "Discontinues"},
+  {name: "Rock hard Senbei", catalogNumber: "5624523", price: 400, availability: "Discontinued"},
   {name: "Fruity Jelly", catalogNumber: "125476435", price: 300, availability: "On Sale"},
   {name: "Chocolate Bucket", catalogNumber: "5234526234", price: 1000, availability: "On Sale"},
   {name: "Happy Biscuit", catalogNumber: "1325345", price: 600, availability: "On Sale"},
