a3e07be2203090d2452ba518e9efecc73ae0c580
Naofumi Kagami <naofumi@mac.com>
2024-07-24 23:45:14 +0900

    wording

--- pages/live_search/index.tsx
+++ pages/live_search/index.tsx
@@ -94,7 +94,7 @@ export default function LiveSearchIndex() {
                   <div className="my-10 flex items-center justify-center gap-x-6">
                     <Link href="/api/hotwire/live_search"
                           className="rounded-md bg-orange-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600">
-                      HotwireによるLive Search</Link>
+                      HotwireによるLive Searchへ</Link>
                   </div>
                 </div>
               </div>
