1fc8722f00e82186b433b99f641ce293f6417628
Naofumi Kagami <naofumi@mac.com>
2024-07-28 12:08:45 +0900

    Use Link instead of a tag

--- components/PrimaryButtonWithLink.tsx
+++ components/PrimaryButtonWithLink.tsx
@@ -1,17 +1,17 @@
 import Link from "next/link";
 import {ReactNode} from "react";
 
-export default function PrimaryButtonWithAnchor({href, children, ...others}: {
+export default function PrimaryButtonWithLink({href, children, ...others}: {
   href: string,
   children: ReactNode,
   [key: string]: any
 }) {
   return (
-    <a href={`${href}`} {...others}
+    <Link href={`${href}`} {...others}
       className="text-sm rounded-md bg-orange-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm
       hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2
       focus-visible:outline-orange-600">
       {children}
-    </a>
+    </Link>
   )
 }
