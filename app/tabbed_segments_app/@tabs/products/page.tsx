6335fb15baea14df82b972ece26a5a1dfc11b75a
Naofumi Kagami <naofumi@mac.com>
2024-07-31 00:47:15 +0900

    remove unnecessary buttons

--- app/tabbed_segments_app/@tabs/products/page.tsx
+++ app/tabbed_segments_app/@tabs/products/page.tsx
@@ -70,11 +70,6 @@ export default async function TabbedSegmentsProductsPage() {
                   </tr>))}
                 </tbody>
               </table>
-              <div className="my-10 flex items-center justify-center gap-x-6">
-                <Link href="/users_app"
-                      className="rounded-md bg-orange-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600">
-                  Usersへ App router</Link>
-              </div>
             </div>
           </div>
         </div>
--- app/tabbed_segments_app/@tabs/users/page.tsx
+++ app/tabbed_segments_app/@tabs/users/page.tsx
@@ -70,11 +70,6 @@ export default async function TabbedSegmentsUsersPage() {
                   </tr>))}
                 </tbody>
               </table>
-              <div className="my-10 flex items-center justify-center gap-x-6">
-                <Link href="/products_app"
-                      className="rounded-md bg-orange-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600">
-                  Productsへ App router</Link>
-              </div>
             </div>
           </div>
         </div>
