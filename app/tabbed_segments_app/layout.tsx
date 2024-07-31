92f9c12fd4cfb4d1796c2c851e68a603718f240c
Naofumi Kagami <naofumi@mac.com>
2024-07-31 22:13:21 +0900

    タブで区切ったUI => タブメニューUI

--- app/tabbed_segments_app/layout.tsx
+++ app/tabbed_segments_app/layout.tsx
@@ -8,7 +8,7 @@ export default function TabbedSegmentsLayout(
       <div className="px-6 py-24 sm:px-6 sm:py-32 lg:px-8">
         <div className="mx-auto max-w-2xl text-center">
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            タブで区切ったUI
+            タブメニューUI
           </h2>
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <a href="/api/hotwire/tabbed_segments_no_js"
