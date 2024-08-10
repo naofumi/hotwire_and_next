1b1e96da031b4225a82dbfe0f6a1e04461d5d58a
Naofumi Kagami <naofumi@mac.com>
2024-08-10 15:59:58 +0900

    Revert "Merge branch 'main' of https://github.com/naofumi/hotwire_and_next"
    This reverts commit 5a703a6989608f32a635ed126482ed836df38906, reversing
    changes made to 3a299eccfd8c73e3b99121d4f18c065c9fa8cbae.

--- pages/api/hotwire/modal_no_js/modal.ts
+++ pages/api/hotwire/modal_no_js/modal.ts
@@ -2,14 +2,16 @@
 import type {NextApiRequest, NextApiResponse} from "next";
 import {render} from "@/helpers/template-renderer"
 
-import {allUsers, searchUsers, User} from "@/repositories/user";
+import {sleep} from "@/helpers/sleep";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<string>,
 ) {
 
-  const resultText = render("modal_no_js/index.ejs",
+  await sleep(2000);
+
+  const resultText = render("modal_no_js/modal.ejs",
     {}
   )
 
