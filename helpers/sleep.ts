699c6f5c16d19886e59df33827ad70c71b71719c
Naofumi Kagami <naofumi@mac.com>
2024-08-11 22:01:27 +0900

    improve delay algorithm

--- helpers/sleep.ts
+++ helpers/sleep.ts
@@ -1,14 +1,12 @@
 export const databaseDelay = parseInt(process.env.DATABASE_DELAY || "0")
-export const responseDelay = parseInt(process.env.RESPONSE_DELAY || "0")
 
 export function sleep(ms: number) {
+  if (ms > 0) {
+    console.log(`sleeping ${ms} ms`)
+  }
   return new Promise(resolve => setTimeout(resolve, ms))
 }
 
 export async function databaseSleep() {
   await sleep(databaseDelay)
-}
-
-export async function responseSleep() {
-  await sleep(responseDelay)
 }
--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,51 @@
 import type {NextRequest} from 'next/server'
 import {NextResponse} from 'next/server'
-import {responseSleep} from "@/helpers/sleep";
+import {sleep} from "@/helpers/sleep";
 
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
-  if (shouldDelayResponse(request)) {
-    await responseSleep()
+  const url = new URL(request.url)
+
+  if (externalRequest(request)) {
+    await sleep(delayToApply(url))
   }
 
   return NextResponse.next()
 }
 
-function shouldDelayResponse(request: NextRequest) {
+function delayToApply(url: URL) {
+  const defaultDemoDelay = 300
+  const pathName = url.pathname
+
+  if (pathName === "/") {
+    return 0
+  } else if (pathName.startsWith("/users")) {
+    return defaultDemoDelay
+  } else if (pathName.startsWith("/products")) {
+    return defaultDemoDelay
+  } else if (pathName.startsWith("/address_selector")) {
+    return defaultDemoDelay
+  } else if (pathName.startsWith("/details_panel")) {
+    return defaultDemoDelay
+  } else if (pathName.startsWith("/live_search")) {
+    return defaultDemoDelay
+  } else if (pathName.startsWith("/modal")) {
+    return defaultDemoDelay
+  } else if (pathName.startsWith("/popup")) {
+    return defaultDemoDelay
+  } else if (pathName.startsWith("/tabbed")) {
+    return defaultDemoDelay
+  } else if (pathName.startsWith("/api")) {
+    return defaultDemoDelay
+  } else {
+    return 0
+  }
+}
+
+function externalRequest(request: NextRequest) {
   const userAgent = request.headers.get('user-agent')
+  const url = request.url
   if (userAgent === "node") {
     console.log("internal API request: no delay")
     // If the userAgent is "node" then this request originated from
