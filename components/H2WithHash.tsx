418bce78790a278410c9fc15e74f36612442e21e
Naofumi Kagami <naofumi@mac.com>
2024-08-15 10:14:29 +0900

    Adjust heading margin

--- components/H2WithHash.tsx
+++ components/H2WithHash.tsx
@@ -3,7 +3,7 @@ import {ReactNode} from "react";
 
 export default function H2WithHash({id, children}: { id: string, children: ReactNode }) {
   return (
-    <h2 className="mt-8 text-2xl font-bold tracking-tight text-gray-900" id={id}>
+    <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900" id={id}>
       {children}
       <Link href={`#${id}`} className="ml-4 text-gray-400 hover:text-orange-600">#</Link>
     </h2>
