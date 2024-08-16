7aa9ae93420d36f256be4631256849301a5c1a1d
Naofumi Kagami <naofumi@mac.com>
2024-08-16 11:39:21 +0900

    suspense for modals

--- app/modal_app_suspense/components/Modal.tsx
+++ app/modal_app_suspense/components/Modal.tsx
@@ -1,28 +1,5 @@
-import React from "react";
-import {cookies} from "next/headers";
-import {revalidatePath} from "next/cache";
-import {redirect} from "next/navigation";
-
-async function getGreeting() {
-  "use server"
-  /* We mimic database access by storing data in cookies */
-  const greeting = cookies().get("greeting")?.value as string | undefined;
-  return greeting
-}
-
-export async function submitHandler(formData: FormData) {
-  "use server"
-  /* We mimic database access by storing data in cookies */
-  const greeting = formData.get("greeting") as string | null;
-  if (greeting) {
-    cookies().set("greeting", greeting)
-  }
-  revalidatePath("/")
-  redirect("/modal_app_suspense")
-}
-
-export default async function Modal() {
-  const currentGreeting = await getGreeting();
+import React, {ReactNode} from "react";
+export default async function Modal({children}: {children: ReactNode}) {
 
   return (
     <div className="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
@@ -33,17 +10,7 @@ export default async function Modal() {
           <div
             className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
             <div className="mt-5 sm:mt-6">
-              <form action={submitHandler}>
-                <label htmlFor="greeting-input" className="text-sm font-bold">Enter new greeting:</label>
-                <input id="greeting-input"
-                       name="greeting"
-                       defaultValue={currentGreeting}
-                       className="mt-2 border rounded p-1 w-full"/>
-                <button type="submit"
-                        className="mt-8 inline-flex w-full justify-center rounded-md bg-orange-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600">
-                  Submit greeting
-                </button>
-              </form>
+              {children}
             </div>
           </div>
         </div>
--- app/modal_app_suspense/show_modal/loading.tsx
+++ app/modal_app_suspense/show_modal/loading.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import rocketImage from "@/public/images/rocket.gif";
+import Modal from "@/app/modal_app_suspense/components/Modal";
 
 export default function Loading() {
   // You can add any UI inside Loading, including a Skeleton.
   return <>
-    <div className="w-full mt-24">
-      <Image src={rocketImage} alt="loader" className="w-16 h-16 mx-auto"/>
-    </div>
+    <Modal>
+      <div className="w-full mt-6">
+        <Image src={rocketImage} alt="loader" className="w-16 h-16 mx-auto"/>
+      </div>
+    </Modal>
   </>
 }
