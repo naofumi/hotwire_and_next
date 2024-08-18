48c9c7e22dc7dbe5a008e2ff7977d74fcd70b92c
Naofumi Kagami <naofumi@mac.com>
2024-08-18 20:11:06 +0900

    rename modal_controller.js to labeler_controller.js to make more generic

---
+++ public/hotwire/javascript/labeler_controller.js
@@ -0,0 +1,19 @@
+import {Controller} from "/hotwire/javascript/stimulus.js"
+
+export default class extends Controller {
+  static values = {"selector": String}
+  static classes = ["label"]
+
+  connect() {
+  }
+
+  add() {
+    const labelable = document.querySelector(this.selectorValue)
+    labelable.classList.add(this.labelClass)
+  }
+
+  remove() {
+    const labelable = document.querySelector(this.selectorValue)
+    labelable.classList.remove(this.labelClass)
+  }
+}
