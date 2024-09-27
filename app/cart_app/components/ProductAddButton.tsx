153953603be672aeb7cef7e235a36371b974a27a
Naofumi Kagami <naofumi@mac.com>
2024-09-27 21:50:33 +0900

    Use bind to add parameters to Server Actions

--- app/cart_app/components/ProductAddButton.tsx
+++ app/cart_app/components/ProductAddButton.tsx
@@ -3,10 +3,10 @@ import {revalidatePath} from "next/cache";
 import {cookies} from "next/headers"
 import {getCookie, setCookie} from "cookies-next"
 
-async function addToCart(formData: FormData) {
+async function addToCart(productId: number, formData: FormData) {
   "use server"
 
-  const productId = formData.get("product_id") as string
+  // const productId = formData.get("product_id") as string
 
   const cartString = getCookie("cart", {cookies})
   const cart = cartString ? JSON.parse(cartString) : {}
@@ -19,8 +19,7 @@ async function addToCart(formData: FormData) {
 
 
 export default function ProductAddButton({product}: { product: Product }) {
-  return (<form action={addToCart}>
-    <input type="hidden" name="product_id" value={product.id} />
+  return (<form action={addToCart.bind(null, product.id)}>
     <button type="submit"
             className="btn-primary border border-orange-600">Add to Cart
     </button>
