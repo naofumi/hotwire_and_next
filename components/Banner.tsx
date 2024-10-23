f5b69048742f3fb1b06125a84b2340d4f5d322d8
Naofumi Kagami <naofumi@mac.com>
2024-10-23 17:11:13 +0900

    documentation

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,7 +1,8 @@
 export default function Banner() {
-  return (
-    <div className="z-20 text-2xl fixed opacity-70 right-0 top-0 w-56 bg-rose-600 text-white font-bold text-center text-white rotate-45 translate-x-12 translate-y-12">
-      beta
-    </div>
-  )
+  return null
+  // return (
+  //   <div className="z-20 text-2xl fixed opacity-70 right-0 top-0 w-56 bg-rose-600 text-white font-bold text-center text-white rotate-45 translate-x-12 translate-y-12">
+  //     beta
+  //   </div>
+  // )
 }
